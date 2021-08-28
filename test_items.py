2d434178b8a3c5270e9e4c7e8e44d250b9949484
dexer <norton@e1.ru>
2021-08-28 14:12:57 +0500

    add add_to_card_button test

---
+++ test_items.py
@@ -0,0 +1,10 @@
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_add_to_cart_button_is_enabled(browser):
+    browser.get(link)
+    add_to_basket_btn = browser.find_element_by_css_selector(
+        '#add_to_basket_form .btn-add-to-basket')
+
+    is_enabled = add_to_basket_btn.is_enabled()
+    assert is_enabled, 'add to basktet button is not enabled'
